8f7f2f0a42b8e12916eb62f47e9b7e637c52c48d
Latif <latifyahia98@gmail.com>
2021-02-18 14:47:03 +0000

    testing add function

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -1,4 +1,9 @@
 package ie.gmit;
 
 public class Calculator {
+    public int Add(int num1, int num2)
+    {
+        int sum = num1 + num2;
+        return sum;
+    }
 }
--- src/test/java/ie/gmit/CalculatorTest.java
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -1,5 +1,17 @@
 package ie.gmit;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
     //this method will add two numbers
+
+    @Test
+    void AddTest()
+    {
+        Calculator CalTest = new Calculator();
+        int sum = CalTest.Add(84,46);
+        assertEquals(130, sum, 0);
+    }
 }
